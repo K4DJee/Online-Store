3603e66f14408ea09e98f5b201fa9645fb793920
Dima <mishchenkodima24@gmail.com>
2025-07-13 14:04:49 +0300

    [fix] '/api/change-product-cart' api endpoint

--- controllers/api-carts-controller.js
+++ controllers/api-carts-controller.js
@@ -63,7 +63,7 @@ const addProductInCart = async (req,res)=>{
 
 const changeQuantityProductsInCart = async (req,res)=>{
     try{
-        const { cartId, quantity} = req.body;
+        const { cartId, quantity, productId} = req.body;
         const authHeader = req.headers['authorization'];
         if(!authHeader){
             return res.status(400).json({message:'Token required', success:false}); 
@@ -79,18 +79,24 @@ const changeQuantityProductsInCart = async (req,res)=>{
         if(!cartId || !quantity){
             return res.status(400).json({message:'Wrong data', success:false});
         }
+        const checkProductQuantityRow = await checkProductQuantitySQL(productId);
+        if(checkProductQuantityRow.quantity < quantity){
+            return res.status(400).json({message:'Количество выбранных товаров превышает количество имеющихся',
+                success:false
+            });
+        }
         const cartItemExists = await checkCartItemExistsSQL(decoded.userId, cartId);
             if(!cartItemExists) {
                 return res.status(404).json({message: 'Товар в корзине не найден', success: false});
             }
+        const quantityInt = parseInt(quantity);
         //checkProductQuantity
-        if(quantity === 0){
+        if(quantityInt === 0){
             const deleteProductInCartRow = await deleteProductInCartSQL(decoded.userId, cartId);
             if(deleteProductInCartRow.affectedRows === 0 || !deleteProductInCartRow){
                 return res.status(500).json({message:'Товар из корзины не был удалён', success:false});
             }
-            return res.status(200).json({message:'Товар был успешно удалён из корзина', success:true,
-                deleteProductInCartRow:deleteProductInCartRow
+            return res.status(200).json({message:'Товар был успешно удалён из корзины', success:true
             })
         }
         const changeQuantityProductsRow = await changeQuantityProductsInCartSQL(decoded.userId, cartId, quantity);
@@ -100,8 +106,7 @@ const changeQuantityProductsInCart = async (req,res)=>{
         if(changeQuantityProductsRow.changedRows === 0){
             return res.status(500).json({message:'Данные товара в корзине не изменились', success:false});
         }
-        return res.status(200).json({message:'Данные товара успешно изменились', success:true, 
-            changeQuantityProductsRow:changeQuantityProductsRow
+        return res.status(200).json({message:'Данные товара успешно изменились', success:true
         })
     }
     catch(error){
